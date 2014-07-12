bbbcfd98fb74b3c37f215df7c94cf728b9f5dcb5
Ivan Tenev <ivtenev@gmail.com>
2014-07-12 17:29:24 +0300

    commit test

---
+++ testNunitSolution/testNunitSolution/Test.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System;
+using consoleApp;
+
+namespace testNunitSolution
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase ()
+		{
+			//Assert.Pass ("pass exception");
+			//Assert.Fail ();
+			//Assert.
+			cl1 cl1Obj = new cl1 ();
+			Assert.IsInstanceOf<cl1> (cl1Obj);
+			Assert.IsTrue (cl1Obj.M1 == 0);
+			Assert.AreEqual (cl1Obj.M1, 1);
+		}
+	}
+}
