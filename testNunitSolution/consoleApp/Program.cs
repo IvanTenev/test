c8cf063e105279616a4c7a7dbe81904f72fc4f59
IvanTenev <ivan.tenev@hotmail.com>
2015-02-07 10:39:33 +0200

    abstract class as interface added

--- testNunitSolution/consoleApp/Program.cs
+++ testNunitSolution/consoleApp/Program.cs
@@ -2,6 +2,84 @@ using System;
 
 namespace consoleApp
 {
+	abstract class abClass
+	{
+		public abstract string GetName ();
+
+	}
+
+	class abChild : abClass
+	{
+		public override string GetName()
+		{
+			//return this.GetType ().ToString ();
+			return("abChild");
+		}
+	}
+
+	class abChild2 : abClass
+	{
+		public override string GetName()
+		{
+			//return this.GetType ().ToString ();
+			return("abChild2");
+		}
+	}
+
+
+
+	static class sClass1 
+	{
+		static int a;
+		static sClass1()
+		{
+			a = 7;
+		}
+		static public void printName()
+		{
+			Console.WriteLine ("sClass1:{0}",a);
+		}
+	}
+
+
+	interface IPrintName
+	{
+		void printName();
+	}
+
+	class A : IPrintName
+	{
+		public void printName()
+		{
+			Console.WriteLine ("A");
+		}
+
+		public virtual void printNameOVR()
+		{
+			Console.WriteLine ("Ao");
+		}
+
+	}
+
+	class B : A , IPrintName
+	{
+		new public void printName()
+		{
+			Console.WriteLine ("B");
+		}
+
+		public override void printNameOVR()
+		{
+			Console.WriteLine ("Bo");
+		}
+
+		public void printNameBase()
+		{
+			base.printName ();
+		}
+
+	}
+
 	public class cl1:IWriteParams
 	{
 		private int m1;
@@ -48,15 +126,56 @@ namespace consoleApp
 
 		public static void Main (string[] args)
 		{
-			cl1 cl1Obj; 
-			cl1Obj = new cl1(); 
-			Console.WriteLine ("Hello World! {0}", cl1Obj.M2);
-			//WriteParams ();
-			MainClass mc = new MainClass ();
-			mc.WriteParams ();
+	//		cl1 cl1Obj; 
+	//		cl1Obj = new cl1(); 
+	//		Console.WriteLine ("Hello World! {0}", cl1Obj.M2);
+	//		//WriteParams ();
+	//		MainClass mc = new MainClass ();
+	//		mc.WriteParams ();
 			//cl1Obj.WriteParams ();
 
-			BNode<int> BNode = new BNode<int>(5);
+	//		BNode<int> BNode = new BNode<int>(5);
+
+			Console.WriteLine ("direct access");
+			sClass1.printName ();
+
+			A a = new A();
+			a.printName ();
+			a.printNameOVR ();
+
+			B b = new B();
+			b.printName();
+			b.printNameOVR ();
+			Console.WriteLine ("Print base.");b.printNameBase ();
+
+			Console.WriteLine ("through reference to the base class+override");
+			A c;
+			c = a;
+			c.printName ();
+			c.printNameOVR ();
+			c = b;
+			c.printName ();
+			c.printNameOVR ();
+
+			Console.WriteLine ("through interface");
+			IPrintName ipn;
+			ipn = a;
+			ipn.printName ();
+			ipn = b;
+			ipn.printName ();
+
+			abClass abc;
+
+			abChild ab0 = new abChild ();
+			abChild2 ab1 = new abChild2 ();
+
+			Console.WriteLine ("through abstract class");
+			abc = ab0;
+			Console.WriteLine (abc.GetName ());
+
+			abc = ab1;
+			Console.WriteLine (abc.GetName ());
+
 		}
 	}
 
